abc565a5aa270929522d9d73923cbd0285a74a4d
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2025-01-20 01:31:15 -0300

    reorganized code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Compilation command
+```
+ gcc -Iinclude main.c src/sphere.c src/lighting.c src/vector.c -o run -lm
+```
--- include/lighting.h
+++ include/lighting.h
@@ -1,6 +1,8 @@
 #ifndef LIGHTING_H
 #define LIGHTING_H
 
+#include "vector.h"
+
 Vector getLightVector(Vector surface, Vector lightPos);
 Vector getReflectionVector(Vector light, Vector normal);
 Vector getObserverVector(Vector surface, Vector observer);
